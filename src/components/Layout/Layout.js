e2880fe36461132025967026d6e8250cf67a01e1
gprc <gprc@myorg>
2021-04-10 20:26:12 -0500

    Fixed Bugs
    Signed-off-by: gprc <gprc@myorg>

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,15 +1,15 @@
 import React from 'react';
 
-import Aux from '../../hoc/Aux';
+import Auxillary from '../../hoc/Auxillary';
 import classes from './Layout.css';
 
 const layout = (props) => (
-    <Aux>
+    <Auxillary>
         <div>Toolbar, SideDrawer, Backdrop</div>
         <main className={classes.Content}>
             {props.children}
         </main>
-    </Aux>
+    </Auxillary>
 );
 
 export default layout;
